e67f7c4d42101b5ce303d89bd584c9dc06812977
Edwin Pers <pers.edwin@gmail.com>
2023-07-11 13:50:59 +0000

    workflows to automatically build and release

--- build-env/build-env-exec.sh
+++ build-env/build-env-exec.sh
@@ -13,11 +13,13 @@ DIRNAME=$(dirname "$0")
 SCRIPT_DIR=$(cd "$DIRNAME" || exit 1; pwd)
 cd "$SCRIPT_DIR" || exit 1
 
+REPO_ROOT_DIR=$(cd "$SCRIPT_DIR"/.. || exit 1; pwd)
+
 podman run \
   --rm \
   -it \
   --uidmap 1000:0:1 \
   --uidmap 0:1:1000 \
-  --volume "/home/epers/repos/UBIBoot:/opt/workdir:rw" \
-  "localhost/ubi-build-env:dev" \
-    /bin/bash
+  --volume "$(echo "$REPO_ROOT_DIR:/opt/workdir:rw")" \
+  "ghcr.io/potassium-os/ubiboot:latest" \
+    /bin/bash -c "$*"
